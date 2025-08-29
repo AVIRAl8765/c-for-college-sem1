e0a0e26d1c4e4a5bc1cfd9ccabfaea0d5ae3dc01
AVIRAl8765 <145892915+AVIRAl8765@users.noreply.github.com>
2025-08-29 15:37:20 +0530

    Add files via upload

---
+++ TEMP_CON.C
@@ -0,0 +1,12 @@
+// program for temprature conversions
+#include <stdio.h>
+#include <conio.h>
+void main(){
+float c , feh;
+clrscr();
+printf("enter the temprature in celcius degree \n");
+scanf("%f",&c);
+feh=1.8*c+35;
+printf("temprature in fehrenhit is %f",feh);
+getch();
+}
